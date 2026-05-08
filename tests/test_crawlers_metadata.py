941a7f2bfd4ed7818791f2e37b1d7723ad5849cc
neviaumi <david.ng.dev@gmail.com>
2026-05-08 15:32:24 +0100

    test: remove The Range crawler from metadata test suite

--- tests/test_crawlers_metadata.py
+++ tests/test_crawlers_metadata.py
@@ -3,7 +3,6 @@ import pytest
 import app.crawlers.diy_dot_com_crawler as diy_dot_com_crawler
 import app.crawlers.homebase_crawler as homebase_crawler
 import app.crawlers.screwfix_crawler as screwfix_crawler
-import app.crawlers.the_range_crawler as the_range_crawler
 import app.crawlers.toolstation_crawler as toolstation_crawler
 import app.crawlers.wickes_crawler as wickes_crawler
 
@@ -11,7 +10,6 @@ CRAWLERS = [
     diy_dot_com_crawler,
     homebase_crawler,
     screwfix_crawler,
-    the_range_crawler,
     toolstation_crawler,
     wickes_crawler,
 ]
